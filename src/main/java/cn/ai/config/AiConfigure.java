f57512b984d7d20401998fd4f7f3a3e6cfb5fe46
wgk <2287334379@qq.com>
2025-08-06 19:07:01 +0800

    yes

--- src/main/java/cn/ai/config/AiConfigure.java
+++ src/main/java/cn/ai/config/AiConfigure.java
@@ -2,6 +2,7 @@ package cn.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.openai.OpenAiChatModel;
@@ -18,7 +19,8 @@ public class AiConfigure {
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor()
 //                        ,
-//                        MessageChatMemoryAdvisor.builder(chatMemory).build())
+//                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+                        )
                 .build();
     }
 

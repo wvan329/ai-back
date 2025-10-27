2b3dcd4d2305aa28fd85ddf05b9761906df56ab7
wgk <2287334379@qq.com>
2025-10-27 21:20:18 +0800

    yes

--- src/main/java/cn/ai/controller/AIController.java
+++ src/main/java/cn/ai/controller/AIController.java
@@ -33,7 +33,7 @@ import java.util.Map;
 @RequestMapping("/ai")
 public class AIController {
 
-    private static final String MODEL = "qwen3-tts-flash";
+    private static final String MODEL = "qwen3-tts";
     @Autowired
     ObjectMapper objectMapper;
     @Autowired
